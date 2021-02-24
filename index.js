99ecded542fd59dd61359d5b66b918323f1cf207
Azam287 <mohdazamnoor28@gmail.com>
2021-02-24 16:58:31 +0530

    adding database

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
-//hey 
+const mongoose = require('mongoose');
+//const dotenv = require('dotenv')
+require('dotenv/config');
+
+//Connect to database
+mongoose.connect(process.env.DATABASE_CONNECT,{ useNewUrlParser: true, useUnifiedTopology: true },()=>console.log("Database is connected"));
+
+//import route
+const authRouter  = require('./routes/auth.js');
+
+//middleware
+app.use('/api/user', authRouter);
+
+//listening server
 app.listen(3000,()=>console.log("server is running"));
---
+++ routes/auth.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.post('/',()=>console.log('register'));
+
+module.exports = router;
