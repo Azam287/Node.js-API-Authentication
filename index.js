5b5e52638bfd3b6d9f76a4889072f67a85da4474
Azam287 <mohdazamnoor28@gmail.com>
2021-02-25 13:20:24 +0530

    Error: No entry created in DB

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-//const dotenv = require('dotenv')
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+//import route
+const authRoute  = require('./routes/auth.js');
+
+
+
+
 require('dotenv/config');
 
 //Connect to database
-mongoose.connect(process.env.DATABASE_CONNECT,{ useNewUrlParser: true, useUnifiedTopology: true },()=>console.log("Database is connected"));
+mongoose.connect(process.env.DATABASE_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    ()=>console.log("Database is connected"));
 
-//import route
-const authRouter  = require('./routes/auth.js');
+//Middleware
+//app.use(express.json());
 
-//middleware
-app.use('/api/user', authRouter);
+
+//Route middleware
+app.use('/api/user', authRoute);
+
+
 
 //listening server
 app.listen(3000,()=>console.log("server is running"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
+const User = require('../model/User.js');
 
-router.post('/',()=>console.log('register'));
+
+
+router.post('/register', async(req,res)=>{
+    //res.send("register");
+    const user  = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        date: req.body.date
+    });
+    try{
+        const savedUser = await user.save();
+        //console.res.send(savedUser);
+        res.json(savedUser);
+    }catch(err){
+        //res.status(400).send(err);
+        //res.status(400).send(err);
+        res.json({message: err});
+    }
+});
+
+
+
 
 module.exports = router;
