8be0cbcdb3b06d29ea273b9cae29e9cfef948d24
Azam287 <mohdazamnoor28@gmail.com>
2021-02-26 12:29:13 +0530

    Added Login module

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const User = require('../model/User.js');
 const bcrypt = require('bcryptjs');
-const {registerValidation} = require('../validation');
+const {registerValidation, loginValidation} = require('../validation');
 
 
 
 
-
+//Registration
 router.post('/register', async (req,res)=>{
 
     //validate the data before we a user
@@ -45,13 +45,28 @@ router.post('/register', async (req,res)=>{
     try{
         const savedUser =  await user.save();
         //console.res.send(savedUser);
-        res.json(savedUser);
+        res.send({user: user._id});
     }catch(err){
         res.status(400).send(err.message);
         //res.json({message: JSON.stringify(err.message)});
     }
 });
 
+//Login
+router.post('/login', async (req,res)=>{
+    //validate data before we create a user
+    const{error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the email exist
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Email or password is wrong')
+    //Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid password')
+
+    res.send('Logged in');
+});
 
 
 
